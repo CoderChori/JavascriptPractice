0e9a9284757c0935141e837f3badcb53adc37dfc
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-02-11 17:28:57 +0000

    Main More Changes in Practic

---
+++ Object.js
@@ -0,0 +1,22 @@
+let a = "Anusuma";
+let b = a + 52;
+console.log(b);
+console.log(typeof (b));
+
+const c = {
+  name: "Anusuma",
+  section: 1,
+  IsMonitor: false
+}
+c['friend'] = "Love";
+console.log(c)
+
+const dict = {
+  appreciate: "to recognize how good someone or something is and to value them or it",
+  encounter: "a meeting, especially one that happens by chance",
+  resilience: "the ability to be happy, successful, etc. again after something difficult or bad has happened",
+  leverage: "the action or advantage of using a lever"
+}
+//ways to call object 
+console.log(dict.encounter);
+console.log(dict['encounter']);
