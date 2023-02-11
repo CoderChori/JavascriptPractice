9da835890d993c52321a033d5ae9227e0d68f4d9
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-02-11 17:24:20 +0000

    MoreChanges

---
+++ SwitchCase.js
@@ -0,0 +1,13 @@
+const a = 'Apple';
+switch (a) {
+  case 'Oranges':
+    console.log('Oranges are in stock');
+    break;
+  case 'Mangoes':
+    Console.log('Mangoes are in stock');
+  case 'Apple':
+    console.log('Apples are in stock');
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
