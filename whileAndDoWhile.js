d31ea6e1ccb462185577a070ca51e16eca69edc0
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-02-13 06:57:39 +0000

    Do while and while loop

---
+++ whileAndDoWhile.js
@@ -0,0 +1,21 @@
+//while loop 
+// let n = prompt("Enter a number:");
+// n = Number.parseInt(n);
+// let i = 0;
+// while(i<n)
+//   {
+//     console.log(i);
+//     i++;
+//   }
+
+//do while loop 
+
+let n1 = prompt("Enter a number:");
+n1 = Number.parseInt(n1);
+let i = 0;
+do
+  {
+    console.log(i);
+    i++;
+  }
+while(i<n1)
