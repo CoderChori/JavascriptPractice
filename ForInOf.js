c4a8f81bc9fb9bbb1b4598e7aebed5faeb57bb1b
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-02-13 06:33:57 +0000

    New concept

---
+++ ForInOf.js
@@ -0,0 +1,20 @@
+let obj={
+    Anusuma: 50,
+      Shiv: 80,
+      Divi: 40,
+      Munnu: 60
+  }
+  for(let a in obj)
+    {
+      console.log("Name is " + a + " marks are: " + obj[a]);
+    }
+  let study={
+    Maths: 1,
+    Reasoning: 3,
+    English: 5,
+    General:10
+  }
+  for(let b of "study")
+    {
+      console.log(b);
+    }
