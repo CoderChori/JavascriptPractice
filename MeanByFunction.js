e0429884068d0524bd9017acdd9bdb469631ab21
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-02-14 17:47:04 +0000

    Function

---
+++ MeanByFunction.js
@@ -0,0 +1,9 @@
+// Normal funtion method
+function mean(a, b, c, d, e) 
+{
+  let avg = (a+b+c+d+e)/5;
+  console.log(avg);
+}
+mean(1,2,3,4,5);
+
+// Note: Arrow function will be written as: const mean= ()=>{}
