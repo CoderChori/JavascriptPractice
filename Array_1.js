d17a8a2873570cc62990cf60976b036a85c903a0
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-02-17 17:23:35 +0000

    Array Concept

---
+++ Array_1.js
@@ -0,0 +1,13 @@
+let StudentMarks_12 = [12, 15, 18, 10, 20, 5, "Not Present"];
+StudentMarks_12[8]= 6;
+StudentMarks_12[0]= 50;
+console.log(StudentMarks_12);
+console.log(StudentMarks_12[0]);
+console.log(StudentMarks_12[1]);
+console.log(StudentMarks_12[2]);
+console.log(StudentMarks_12[3]);
+console.log(StudentMarks_12[4]);
+console.log(StudentMarks_12[5]);
+console.log(StudentMarks_12[6]);
+console.log(StudentMarks_12[7]);
+console.log(StudentMarks_12.length);
